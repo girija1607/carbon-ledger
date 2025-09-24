ac8f30ba791ce3ee3b93b4ae52822036384a7f67
Girija <girijasinghal1607@gmail.com>
2025-09-24 23:12:09 +0530

    Test API endpoint to debug 404 error

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
+// Final test api/index.js
+// Ismein Express ya Mongoose kuch bhi nahi hai
 
+module.exports = (req, res) => {
+  // CORS header manually add kar rahe hain
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
 
-app.get('/api/records', (req, res) => {
-  res.status(200).json({ message: 'API is working!' });
-});
-
-
-module.exports = app;
+  // Seedha ek JSON response bhej rahe hain
+  res.status(200).json({
+    message: "Serverless function is working directly!",
+    path: req.url
+  });
+};
